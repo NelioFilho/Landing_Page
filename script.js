0b1de2a639325a86cf072843ee657809979726d8
Nélio <neliofilhodg@outlook.com>
2025-03-30 14:21:23 -0300

    Add scritp e layout projetos

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+var web1 = document.getElementsByClassName('service')[0];
+var web2 = document.getElementsByClassName('service')[1];
 
-setInterval(function(){
-    nextImage();
-}, 2500)
 
-function nextImage(){
-    count ++;
-    if (count>4) {
-        count = 1;
-    }
+var textElement1 = web1.getElementsByTagName('p')[0];
+var textElement2 = web2.getElementsByTagName('p')[0];
 
-    document.getElementById("radio"+count).checked = true;
+function enterMouse1() {
+    textElement1.innerText = 'Uma landing page converte visitantes em clientes rapidamente, enquanto um site profissional transmite credibilidade e fortalece sua presença online. Juntos, eles maximizam suas vendas!';
+    textElement1.style.color = 'black';
+}
+
+function outMouse1() {
+    textElement1.innerText = 'Landing Page | Sites';
+    textElement1.style.color = 'black';
+}
+
+function enterMouse2() {
+    textElement2.innerText = 'Atraia o público certo para o seu negócio com anúncios otimizados. Mais visitantes qualificados = mais vendas!';
+    textElement2.style.color = 'black';
+}
+
+function outMouse2() {
+    textElement2.innerText = 'Tráfego Pago';
+    textElement2.style.color = 'black';
 }
